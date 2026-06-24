a3a85e898fe4f8ea2875f44418c83b6a1a9e9f22
Ludmilajohnston <ludmilavitoria512@gmail.com>
2026-06-24 15:03:14 -0300

    Add files via upload

---
+++ TarefasInteligentes/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'tarefas-inteligentes-v2';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => {
+
+        return response || fetch(event.request);
+      })
+  );
+});
